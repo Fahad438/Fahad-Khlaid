fd2ca6aa0b3baa7773cf2c3c631201253e7073c8
Fahad438 <FAHADKHALID606@GMAIL.COM>
2024-05-11 17:09:15 +0300

    nav and hero done

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#0d1224]`}>
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white ">{children}</main>
+        </body>
     </html>
   );
 }
