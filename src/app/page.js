6ac5fede24554c972ce063557322fcfe5ecac4c2
Omar <bashmailomar@gmail.com>
2024-05-12 05:58:09 +0300

    I just added experiance, skills, projects and contact sections desigin without using any api. Scout!

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,21 @@ import Image from "next/image";
 import { Hero } from "./components/Hero";
 import { Nav } from "./components/Nav";
 import { About } from "./components/About";
-
-
+import { Experience } from "./components/Experience";
+import { Skills } from "./components/Skills";
+import { ContactSection } from "./components/contact/Contact";
+import { Projects } from "./components/projects/Projects";
 
 export default function Home() {
   return (
-<div>
-  <Nav></Nav>
- <Hero></Hero>
- <About></About>
-</div>
+    <div>
+      <Nav></Nav>
+      <Hero></Hero>
+      <About></About>
+      <Experience />
+      <Skills />
+      <Projects />
+      <ContactSection />
+    </div>
   );
 }
